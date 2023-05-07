a4186a8e3bc6eabda65d58a0b5b565a5017d8763
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-07 20:53:05 +0100

    update

---
+++ js/main.vue
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+        data(){
+           return {
+            contacts : [],
+            contact : {id : '',name : '',tel : ''},
+           }
+        },
+        created() {
+         this.getContacts();
+     },
+     methods: {
+         getContacts(){
+             axios.get('http://localhost/crud_app_php_vue/getContacts.php')
+                 .then(response => this.contacts = response.data)
+                 .catch(err => console.log(err));
+         }
+      }
+   })
+     
+app.mount('#app')
