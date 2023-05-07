f32c2fe1a751a652e03aee666d8697f2b3285eb7
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-07 20:22:52 +0100

    update

---
+++ getContact.php
@@ -0,0 +1,18 @@
+<?php
+require('./db/connexion.php');
+
+$sql = "SELECT* FROM contacts";
+$result = mysqli_query($con,$sql);
+
+if($result = mysqli_query($con,$sq)){
+        $i = 0 ;
+        $contactTab = [];
+        while($row = mysqli_fetch_assoc($result)){
+                $contactTab[$i]['id'] = $row['id'];
+                $contactTab[$i]['name'] = $row['name'];
+                $contactTab[$i]['tel'] = $row['tel'];
+                $i++;
+        }
+        echo json_encode($contactTab);
+}
+?>
